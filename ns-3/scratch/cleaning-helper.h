0b1d6d5d1f8d15fa2c5dd9e254c809a327bfb41e
asljivo1 <asljivo1@etf.unsa.ba>
2018-05-16 01:15:19 +0200

    cleaning helper file added in scratch to be included to the libraries. Dependencies fixed.

---
+++ ns-3/scratch/cleaning-helper.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#ifndef CLEANING_HELPER_H
+#define CLEANING_HELPER_H
+
+template <class T> void unused(T &&) {}
+
+#endif /* CLEANING_HELPER_H */
+
+/* paste this file to /usr/local/include/ */
+
